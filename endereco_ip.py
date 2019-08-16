30ab74e92f29627771ec8154528130a360b1e675
Pablo1012 <48845018+Pablo1012@users.noreply.github.com>
2019-08-16 17:02:42 -0300

    Curso Pythonics
    Aula 05 16/08

---
+++ endereco_ip.py
@@ -0,0 +1,5 @@
+ip = input("Digite um endereco: ")
+print('o ip digitado '+ip+' e valido')
+print ('o ip digitado %s e valido' %ip)
+print ('o ip %s e valido e possue %s caracteres' %(ip, str(len(ip))))
+print ('Numero quebrado %f ' % 41.2345)
